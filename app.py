eb1cc8ef23b51b1668d3868effef67dbe606943a
Anshul01R <anshul95858@gmail.com>
2024-09-26 00:29:14 +0530

    Project Completed

---
+++ app.py
@@ -0,0 +1,38 @@
+# app.py
+from flask import Flask, render_template, request, jsonify
+import os
+from TextSummarizer.pipeline.prediction import PredictionPipeline
+
+app = Flask(__name__, static_folder="static", template_folder="static")
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/train', methods=['GET'])
+def train():
+    try:
+        # Replace with actual training script call
+        os.system("python main.py")
+        return "Training successful !!"
+    except Exception as e:
+        return f"Error Occurred: {e}"
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    try:
+        # Extract the text from the request
+        data = request.json
+        text = data.get('text', '')
+
+        # Initialize the Prediction Pipeline and perform prediction
+        obj = PredictionPipeline()
+        summary = obj.predict(text)
+
+        # Return summary as JSON response
+        return jsonify({'summary': summary})
+    except Exception as e:
+        return jsonify({'error': str(e)})
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
---
+++ src/TextSummarizer/pipeline/prediction.py
@@ -0,0 +1,25 @@
+from TextSummarizer.config.configuration import ConfigurationManager
+from transformers import AutoTokenizer
+from transformers import pipeline
+
+
+class PredictionPipeline:
+    def __init__(self):
+        self.config = ConfigurationManager().get_model_evaluation_config()
+
+
+     
+    def predict(self,text):
+        tokenizer = AutoTokenizer.from_pretrained(self.config.tokenizer_path)
+        gen_kwargs = {"length_penalty": 1.5, "num_beams":8, "max_length": 30}
+
+        pipe = pipeline("summarization", model=self.config.model_path,tokenizer=tokenizer)
+
+        print("Dialogue:")
+        print(text)
+
+        output = pipe(text, **gen_kwargs)[0]["summary_text"]
+        print("\nModel Summary:")
+        print(output)
+
+        return output
